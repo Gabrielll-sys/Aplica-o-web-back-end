06f504be27d5692e19b373f8549e216921c09497
Gabriel Lopes costa <gabrielpuneco@gmail.com>
2022-04-23 12:15:38 -0300

    Adicionar arquivos de projeto.

---
+++ Aplicação web back-end/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aplicação_web_back_end.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
